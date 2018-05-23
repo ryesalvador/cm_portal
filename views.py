67e3171e05fa2267c78e4221dd5499c12edd8ab9
Rye Salvador <salvadorrye@gmail.com>
2018-05-23 13:50:19 +0800

    Create a home page

--- views.py
+++ views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Resident, Physician, Relative
 
-# Create your views here.
+def index(request):
+    num_residents = Resident.objects.all().count()
+    num_physicians = Physician.objects.all().count()
+    num_relatives = Relative.objects.all().count()
+
+    return render(request,
+            'cm_portal/index.html',
+            context={
+                'num_residents': num_residents,
+                'num_physicians': num_physicians,
+                'num_relatives': num_relatives
+                })
+
+

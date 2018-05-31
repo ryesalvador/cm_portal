a89bdf47bd8474f536f7822fbcb172663110f0ce
Rye Salvador <salvadorrye@gmail.com>
2018-05-31 09:06:52 +0800

    Separate and add url, view and templates for new service

--- urls.py
+++ urls.py
@@ -1,12 +1,24 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('residents/', views.ResidentListView.as_view(), name='residents'),
-    path('resident/<int:pk>', views.ResidentDetailView.as_view(), name='resident-detail'),
-    path('physicians/', views.PhysicianListView.as_view(), name='physicians'),
-    path('physician/<int:pk>', views.PhysicianDetailView.as_view(), name='physician-detail'),
-    path('relatives/', views.RelativeListView.as_view(), name='relatives'),
-    path('relative/<int:pk>', views.RelativeDetailView.as_view(), name='relative-detail'),
+    path('', views.index, name='index'),    
+]
+
+urlpatterns += [
+    path('nursing-home/', views.nursing_home_index, name='nursing-home-index'),
+    path('nursing-home/residents/', views.ResidentListView.as_view(), name='residents'),
+    path('nursing-home/resident/<int:pk>', views.ResidentDetailView.as_view(), name='resident-detail'),
+    path('nursing-home/physicians/', views.PhysicianListView.as_view(), name='physicians'),
+    path('nursing-home/physician/<int:pk>', views.PhysicianDetailView.as_view(), name='physician-detail'),
+    path('nursing-home/relatives/', views.RelativeListView.as_view(), name='relatives'),
+    path('nursing-home/relative/<int:pk>', views.RelativeDetailView.as_view(), name='relative-detail'),
+]
+
+urlpatterns += [
+    path('hris/', views.hris_index, name='hris-index'),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- views.py
+++ views.py
@@ -4,18 +4,24 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 #from django.contrib.auth.mixins import PermissionRequiredMixin
 
-def index(request):
+def index(request):    
+    return render(request, 'cm_portal/index.html')
+
+def nursing_home_index(request):
     num_residents = Resident.objects.all().count()
     num_physicians = Physician.objects.all().count()
     num_relatives = Relative.objects.all().count()
-
+    
     return render(request,
-            'cm_portal/index.html',
-            context={
-                'num_residents': num_residents,
-                'num_physicians': num_physicians,
-                'num_relatives': num_relatives
-                })
+                  'cm_portal/nursing_home_index.html',
+                  context={
+                      'num_residents': num_residents,
+                      'num_physicians': num_physicians,
+                      'num_relatives': num_relatives
+                      })
+
+def hris_index(request):
+    return render(request, 'cm_portal/hris_index.html')
 
 class ResidentListView(LoginRequiredMixin, generic.ListView):
     model = Resident

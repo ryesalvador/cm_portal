ccc0043e0122d964e9fe38efe39eeba8fd349e07
Rye Salvador <salvadorrye@gmail.com>
2018-09-07 11:38:51 +0800

    Add field indication to drug, alter fields brand_name and dosage on drug

--- models.py
+++ models.py
@@ -49,8 +49,9 @@ GRADE = (
 
 class Drug(models.Model):
     generic_name = models.CharField(max_length=35)
-    brand_name = models.CharField(max_length=35, null=True, blank=True)
-    dosage = models.CharField(max_length=35, null=True, blank=True)    
+    brand_name = models.CharField(max_length=35, default='', blank=True)
+    dosage = models.CharField(max_length=35, default='', blank=True)
+    indication = models.CharField(max_length=70, blank=True)
     price = models.FloatField(null=True, blank=True)
 
     def __str__(self):

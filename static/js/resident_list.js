44fc18624b5657df77d47b3ff708651838269926
Rye Salvador <salvadorrye@gmail.com>
2018-11-03 14:45:53 +0800

    Another try to optimize residents list with ajax

--- static/js/resident_list.js
+++ static/js/resident_list.js
@@ -3,10 +3,30 @@ function sort_by_bldg() {
 	$("#list-by-bldg").show()	
 }
 
+
+
 $(document).ready(function () {
 		$("#list-by-bldg").hide();
-		$("#link-bldg").click(sort_by_bldg)
-		$(".loadlater").each(function(index, element){
-        $(element).attr("src", $(element).attr("data-src"));
-    });
+		$("#link-bldg").click(sort_by_bldg);
+		
+		
+      
+		$(".container").each(function (index, element) {
+			var res = {
+	       loader: $('<i class="fas fa-spinner fa-pulse"></i>'),
+	       image: $(element).find("img") 	
+         }	
+			$.ajax({				
+				url: "#",
+				beforeSend: function () {
+					$(element).find("img").hide();		
+					$(element).prepend(res.loader);					
+				},
+				success: function (data) {
+					$(element).find("img").show();
+					$(element).find(res.loader).remove();
+				}
+			});
+			
+		});
 	});

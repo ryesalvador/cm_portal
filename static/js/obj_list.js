ca2bce8eec53688edc9c7ce47d437b9f6ae6f8b6
Rye Salvador <salvadorrye@gmail.com>
2018-11-06 10:33:46 +0800

    Fix broken pagination

--- static/js/obj_list.js
+++ static/js/obj_list.js
@@ -1,4 +1,5 @@
 function obj_list_page(page_num) {
     $("#obj_list").load("?ajax&page=" + encodeURIComponent(page_num));
+    $("#pagination").load("?ajax&page=" + encodeURIComponent(page_num))
     return false;
 }
--- static/js/resident_list.js
+++
@@ -1,4 +0,0 @@
-function obj_list_page(page_num) {
-    $("#obj_list").load("?ajax&page=" + encodeURIComponent(page_num));
-    return false;
-}

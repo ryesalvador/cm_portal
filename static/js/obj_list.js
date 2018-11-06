edf3098a49e10fd65b50df59a848756311da71cd
Rye Salvador <salvadorrye@gmail.com>
2018-11-06 10:39:25 +0800

    Remove 1 line of code in javascript

--- static/js/obj_list.js
+++ static/js/obj_list.js
@@ -1,5 +1,4 @@
 function obj_list_page(page_num) {
-    $("#obj_list").load("?ajax&page=" + encodeURIComponent(page_num));
-    $("#pagination").load("?ajax&page=" + encodeURIComponent(page_num))
+    $("#obj_list").load("?ajax&page=" + encodeURIComponent(page_num));    
     return false;
 }

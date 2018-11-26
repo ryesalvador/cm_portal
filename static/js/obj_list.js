ae10c2d962c3aa8995d417831e1fd412484818d3
Rye Salvador <salvadorrye@gmail.com>
2018-11-27 01:01:05 +0800

    Jquery Ajax Progress

--- static/js/obj_list.js
+++ static/js/obj_list.js
@@ -1,4 +1,26 @@
 function load_list(id, query, val) {
-    $(id).load("?" + encodeURIComponent(query) + "=" + encodeURIComponent(val));
-    return false;
+	 $.ajax({
+	 	url: "?" + encodeURIComponent(query) + "=" + encodeURIComponent(val),
+	 	beforeSend: function () {
+	 		//$('.progress').hide();
+	 	},
+	 	success: function (data) {
+	 		$(id).html(data);
+	 		//$('.progress').show();
+	 	},
+                progress: function(e) {
+        //make sure we can compute the length
+        if(e.lengthComputable) {
+            //calculate the percentage loaded
+            var pct = (e.loaded / e.total) * 100;
+
+            //log percentage loaded
+            console.log(pct);
+        }
+        //this usually happens when Content-Length isn't set
+        else {
+            console.warn('Content Length not reported!');
+        }
+    }
+	 });	 
 }
